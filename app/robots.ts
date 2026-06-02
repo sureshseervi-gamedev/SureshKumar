e00002a29ef2f7be81f08f232c906d79291db3e7
unknown <suresh.seervi.d@gmail.com>
2026-06-02 20:04:15 +0530

    First commit

---
+++ app/robots.ts
@@ -0,0 +1,14 @@
+// import { MetadataRoute } from 'next'
+
+// export default function robots(): MetadataRoute.Robots {
+//   const baseUrl = 'SureshKumar'
+  
+//   return {
+//     rules: {
+//       userAgent: '*',
+//       allow: '/',
+//       disallow: ['/api/', '/private/'], // Add paths you want to block from crawlers
+//     },
+//     sitemap: `${baseUrl}/sitemap.xml`,
+//   }
+// }
